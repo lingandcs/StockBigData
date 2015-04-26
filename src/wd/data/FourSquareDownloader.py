cc18c3a63c1abc733c3437348a70d6a7463cd410
lingandcs <lingandcs@gmail.com>
2015-04-26 15:36:55 -0400

    added plot; added stock price;

--- src/wd/data/FourSquareDownloader.py
+++ src/wd/data/FourSquareDownloader.py
@@ -12,32 +12,43 @@ import urllib2
 import sys
 import json
 
+
 apiPrefixReview_FourSquare = "https://api.foursquare.com/v2/venues/"
 apiSurfixReview_FourSquare = "/tips?sort=recent&limit=500&client_id=G3RGK41B5M5QZWCPY23IQKXZDYK3LHT4TA2HDTAGFL4NFZTD&client_secret=4BF4J3JGWWBKLMEEWVHVZ2P1VFQGJXTBBLRZU0FVSMH4TAHM"
+
 
 
 if __name__ == '__main__':
     conn = mysql.connector.connect(user='lingandcs', password='sduonline',
                               host='107.170.18.102',
                               database='goodfoodDB')
-    query_insert = "INSERT INTO FinanceNLP_review_FS(id,bizName,createAt,bizSrcID) "  \
+    query_insert = "INSERT INTO goodfoodDB.FinanceNLP_review_FS (id,bizName,createAt,bizSrcID) "  \
                     "VALUES (%s,%s,%s,%s)"
-    
+
     try:
         cursor = conn.cursor()
         cursor_insert = conn.cursor()
         
+        #Hard code biz name, to be changed
         cursor.execute("""
             SELECT * FROM goodfoodDB.goodfood_biz_FourSquare
             WHERE bizName = 'Starbucks'
         """)
+        
         result = cursor.fetchall()
+        print "%d business loaded!"% len(result)
         bizIDs = [row[3] for row in result]
 #         print bizIDs
         today = time.strftime("%Y%m%d")
+        countReview = 0
+        countBiz = 0
+        
         for biz in result:
+            countBiz += 1
+            if countBiz % 100 == 0:
+                print "%dth biz processed"% countBiz
             bizSrcID = biz[3]
-            bizName = biz[1]
+            bizName = biz[1] 
             apiURL = apiPrefixReview_FourSquare + bizSrcID + apiSurfixReview_FourSquare + "&v=" + today
 #             print apiURL
             try:
@@ -49,18 +60,23 @@ if __name__ == '__main__':
                     createAt = review['createdAt']
                     canonicalUrl = review['canonicalUrl']
                     createAt = time.strftime('%Y-%m-%d', time.localtime(int(createAt)))
-                    print reviewID, bizName, createAt, bizSrcID
-                    cursor_insert.execute(cursor_insert, ("test","test","2014-01-01","test"))
-                    cursor_insert.execute(cursor_insert, (u"test",u"test",u"2014-01-01",u"test"))
-                    
-                    cursor_insert.execute(cursor_insert, (reviewID, bizName, createAt, bizSrcID))
-                    
-                    conn.commit()
+#                     print reviewID, bizName, createAt, bizSrcID
+                    try:
+                        cursor_insert.execute(query_insert, (reviewID, bizName, createAt, bizSrcID))
+                        print cursor.lastrowid
+                        countReview += 1
+                        if countReview%1000 == 0:
+                            print '%d reviews inserted'% countReview
+                    except mysql.connector.errors.IntegrityError:
+#                         print 'fuck, duplicate key!!!'
+                        pass
+#                     conn.commit()
 #                 print len(reviews)
-                    sys.exit()
+#                     sys.exit()
             except urllib2.HTTPError:
-                print 'fuck'
+                print 'fucking network!'
 
         
     finally:
+        conn.commit()
         conn.close()
